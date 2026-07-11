dc71cd83ff3b9b87322850d7b544e33ca1f4d570
Sougat <sougat711@gmail.com>
2026-07-11 02:47:49 +0000

    feat: add protected/api/me/ endpoint using JWT authentication

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 from rest_framework import generics
 from .serializers import RegisterSerializer
-
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
 # Create your views here.
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
+
+
+class MeView(APIView):
+    permission_classes =[IsAuthenticated]
+    def get(self,request):
+        user = request.user
+        
+        return Response({
+            'id':user.id,
+            'username':user.username,
+            'email': user.email,
+            'phone_number': 'user.phone_number',
+        })
--- backend/ecommerce_core/urls.py
+++ backend/ecommerce_core/urls.py
@@ -17,13 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from accounts.views import RegisterView
+from accounts.views import RegisterView, MeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('api/register/',RegisterView.as_view(),name= 'register'),
-
+    path('api/me/',MeView.as_view(), name='me'),
 
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
